4e52eb435918f27044c00951c073b11891a7240b
NataliyaKiseleva <111054659+NataliyaKiseleva@users.noreply.github.com>
2022-10-05 22:26:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿string[] Array = { "HELLO", "<->", "World", ":)" };
+string newArray = string.Join(", ", Array);
+Console.Write($"[{newArray}] -> ");
+
+for (int i = 0; i < Array.Length; i++){
+    if (Array[i].Length <= 3){
+        if (i != Array.Length - 1){
+            Console.Write("[");
+            Console.Write($"{Array[i]}, ");
+        }
+        else{
+            Console.Write($"{Array[i]}");
+            Console.Write("]");
+        }
+    }
+}
+
